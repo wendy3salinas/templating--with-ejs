7c8ac56bf887486d5eb1d973353b03b5314115c6
Wendy Salinas-Reyes <wendy3salinas@gmail.com>
2022-03-14 06:03:32 +0000

    adds more pages using express to change content

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = 'Our home page';
+  res.render('pages/index', {title:title});
+})
+
+//our crochet page
+app.get('/crochet', (req, res) => {
+  var title = 'Our Crochet page';
+  res.render('pages/Crochet', {title:title});
+})
+
+//our sewing page
+app.get('/sewing', (req, res) => {
+  var title = 'Our Sewing page';
+  res.render('pages/Sewing', {title:title});
+})
+
+//our book page
+app.get('/books', (req, res) => {
+  var title = 'Our Books Page';
+  res.render('pages/Books', {title:title});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
+
