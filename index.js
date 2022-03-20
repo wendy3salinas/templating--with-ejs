28e4e3390e7834bd4ac10ccd18e27298389e47b4
Wendy Salinas-Reyes <wendy3salinas@gmail.com>
2022-03-20 23:19:29 +0000

    adds ejs pages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var data = require('./data/test.json');
+
 app.set('view engine', 'ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
@@ -31,7 +33,28 @@ app.get('/books', (req, res) => {
   res.render('pages/Books', {title:title});
 })
 
+//users index is our list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+    console.log(data);
+});
 
